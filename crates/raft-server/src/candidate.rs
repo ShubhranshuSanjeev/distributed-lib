c0b380e62dccb6a3c89913d0f3067c60cca8c50c
Shubhranshu Sanjeev <shubhranshu.sanjeev@gmail.com>
2026-06-21 06:16:53 +0530

    feat: first cut usage of client for request vote rpc

---
+++ crates/raft-server/src/candidate.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+use crate::raft_context::Raft;
+
+pub struct Candidate;
+
+// impl Candidate {
+//     fn conduct_election(raft: &mut Raft<Candidate>) {
+//
+//     }
+//     fn retry_election(raft: &mut Raft<Candidate>) {
+//
+//     }
+//     fn transition_to_leader(raft: &mut Raft<Candidate>) {
+//
+//     }
+//     fn transition_to_follower(raft: &mut Raft<Candidate>) {
+//
+//     }
+// }
