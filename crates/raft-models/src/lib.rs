520a95fa9669502dcc1966c630c597bff2deafbf
Shubhranshu Sanjeev <shubhranshu.sanjeev@gmail.com>
2026-06-24 00:55:43 +0530

    refactor: separate raft-core and raft-server crate (missed files)

--- crates/raft-models/src/lib.rs
+++ crates/raft-models/src/lib.rs
@@ -1,6 +1,6 @@
 use prost::Message;
 
-use crate::state::PersistentState;
+use crate::state::{PersistentState, ServerState};
 
 pub mod common {
     include!("../generated/raft.common.rs");
@@ -16,5 +16,25 @@ impl PersistentState {
     pub fn load_from_file(path: &str) -> Result<Self, prost::DecodeError> {
         let data: Vec<u8> = std::fs::read(path).unwrap();
         PersistentState::decode(data.as_slice())
+    }
+}
+
+impl ServerState {
+    pub fn from_persistent_state(path: &str) -> Self {
+        match PersistentState::load_from_file(path) {
+            Err(e) => {
+                println!("failed to load persistent state {}", e);
+                Self::default()
+            }
+            Ok(ps) => Self {
+                current_term: ps.current_term,
+                voted_for: ps.voted_for,
+                log: ps.log,
+                // TODO: there should be some relation between the logs
+                // and then these commit_index and last_applied fields
+                commit_index: 0,
+                last_applied: 0,
+            },
+        }
     }
 }
--- crates/raft-server/src/lib.rs
+++ crates/raft-server/src/lib.rs
@@ -1,13 +1,6 @@
 pub use raft_models::rpc as rpc_models;
 
-pub mod service {
+pub mod server {
     include!("../generated/raft.service.rs");
 }
-pub mod candidate;
-pub mod election;
-pub mod follower;
-pub mod leader;
-pub mod raft_context;
-pub mod state_transition_result;
-pub mod cluster_config;
-pub mod utils;
+pub mod service;
